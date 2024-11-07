1e7d77227af6568ffb61f60a62f2973a48aa5d80
Nato Boram <nato.boram+git@gmail.com>
2024-11-07 12:23:34 -0500

    🏷️ Patch @ast-grep/napi

--- src/not_assignable.test.ts
+++
@@ -1,6 +0,0 @@
-import { test } from "vitest"
-import { lang } from "./not_assignable.ts"
-
-test("not assignable", ({ expect }) => {
-	expect(lang).toBe("TypeScript")
-})
--- src/not_assignable.ts
+++
@@ -1,4 +0,0 @@
-import type { Lang } from "@ast-grep/napi"
-
-/** Type `"TypeScript"` is not assignable to type `Lang`. */
-export const lang: Lang = "TypeScript"
